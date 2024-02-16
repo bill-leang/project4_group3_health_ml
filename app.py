f668474f2b92303dd82e5c51076f12cd9225a2b4
bill-leang <65846978+bill-leang@users.noreply.github.com>
2024-02-16 11:59:51 +1100

    first draft app.py

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello,World!</p>"
+    # file must be in templates folder
+    return render_template("index.html")
 
-# will be able to use <name> in code
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+@app.route("/login", methods=["POST", "GET"])
+def login():
+  if request.method == "POST":
+    # get the value nm from login.html
+    age = request.form["age"]
+    return redirect(url_for("user", age=age))
+  else:
+    return render_template("login.html")
 
-# using redirect
-@app.route("/admin")
-def admin():
-    # use the function name in url_for
-    # return redirect(url_for("hello_world"))
+@app.route("/<age>")
+def user(age):
+    return f"<h1>prediction = {predict(age)}</h1>"
 
-    # passing an argument in url_for
-    return redirect(url_for("user",name="Admin!"))
+def predict(age ):
+   return age*365
 
 # this is needed
 if __name__== '__main__':
     app.run(debug=True)
 
 #in the same folder, > env FLASK_APP=app.py python -m flask run OR >python app.py
-# save the file to refresh the server, no need to restart it.
+# save the file to refresh the server, no need to restart it, because debug=True in app.run()
